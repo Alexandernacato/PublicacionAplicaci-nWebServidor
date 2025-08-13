5a6fd0c1f28c3df850c6abe7e1ab193937b96a4a
Alexandernacato <alexandernacato@gmail.com>
2025-08-12 19:35:44 -0500

    Actualizacion

---
+++ Catalogo/src/environments/enviromentc.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://52.14.208.254:8084/api'  // para microservicio categoria, puerto 8084
+};
---
+++ Catalogo/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://52.14.208.254:8082/api' // o como sea tu endpoint real
+};
